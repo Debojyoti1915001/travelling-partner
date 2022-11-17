808e1d592a9b7434ca053abcd81d17267570c300
Debojyoti1915001 <debojyoti76@gmail.com>
2022-11-18 00:11:09 +0530

    POST schema

---
+++ backend/src/models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Post = mongoose.model(
+    "Post",
+    mongoose.Schema({
+        name: { type: String },
+        url:{type: String},
+        percentageWaste:{type: Number},
+        locality: { type: String },
+        city: { type: Number },//0 for user and 1 for worker
+        pin: { type: String },
+        state: { type: String },
+        country:{type: String}
+    })
+);
+module.exports = Post;
